080011c5c779a29ac207ee3c2fcbf8f1ccff942d
LightningTipBot <hWOofXvq4m@mail.com>
2021-08-24 01:38:31 +0200

    botfather commands

--- help.go
+++ help.go
@@ -8,11 +8,11 @@ import (
 const (
 	helpMessage = "ℹ️ *Info*\n_This bot is a Bitcoin Lightning ⚡️ wallet that can sends tips on Telegram. To tip, add the bot to a group chat. The basic unit of tips are Satoshis (sat). 100,000,000 sat = 1 Bitcoin. There will only ever be 21 Million Bitcoin. Type /info for more._\n\n" +
 		"❤️ *Donate*\n" +
-		"_This bot charges no fees but has running costs. If you like the bot, please consider supporting it with a donation. To donate, just tip @LightningTipBot or try_ `/send 1000 @LightningTipBot`\n\n" +
+		"_This bot charges no fees but costs satoshis to operate. If you like the bot, please consider supporting it with a donation. To donate, just tip @LightningTipBot or try_ `/send 1000 @LightningTipBot`\n\n" +
 		"⚙️ *Commands*\n" +
 		"*/tip* 🏅 Reply to a message to tip: `/tip <amount> [<memo>]`\n" +
 		"*/balance* 👑 Check balance: `/balance`\n" +
-		"*/send* 💸 Send funds to a Telegram user: `/send <amount> <@username> [<memo>]`\n" +
+		"*/send* 💸 Send funds to a Telegram user: `/send <amount> <@user> [<memo>]`\n" +
 		"*/invoice* ⚡️ Receive over Lightning: `/invoice <amount> [<memo>]`\n" +
 		"*/pay* ⚡️ Pay over Lightning: `/pay <invoice>`\n" +
 		"*/help* 📖 Read this help.\n"
@@ -32,7 +32,7 @@ const (
 		// "🏛 *Terms*\n" +
 		// "_We are not custodian of your funds. Any amount you load onto your wallet will be legally considered a donation that belongs to us. We will act in your best interest but we're also aware that the situation without KYC is tricky until we figure something out. Do not give us all your money.  Be aware that this bot is in beta development. Use at your own risk._\n\n" +
 		"❤️ *Donate*\n" +
-		"_This bot charges no fees but has running costs. If you like the bot, please consider supporting it with a donation. To donate, just tip @LightningTipBot or try_ `/send 1000 @LightningTipBot`"
+		"_This bot charges no fees but costs satoshis to operate. If you like the bot, please consider supporting it with a donation. To donate, just tip @LightningTipBot or try_ `/send 1000 @LightningTipBot`"
 )
 
 func (bot TipBot) helpHandler(m *tb.Message) {
