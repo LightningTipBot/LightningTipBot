a59b6942eedd1008716d9fd03ada16a7d47595ec
LightningTipBot <hWOofXvq4m@mail.com>
2021-08-25 01:22:33 +0200

    force-init only on / commands

--- text.go
+++ text.go
@@ -14,10 +14,12 @@ func (bot TipBot) anyTextHandler(m *tb.Message) {
 	if m.Chat.Type != tb.ChatPrivate {
 		return
 	}
-	// check if user is in database, if not, initialize wallet
-	if !bot.UserHasWallet(m.Sender) {
-		log.Infof("User %s has no wallet, force-initializing", GetUserStr(m.Sender))
-		bot.startHandler(m)
+	if strings.HasPrefix(m.Text, "/") {
+		// check if user is in database, if not, initialize wallet
+		if !bot.UserHasWallet(m.Sender) {
+			log.Infof("User %s has no wallet, force-initializing", GetUserStr(m.Sender))
+			bot.startHandler(m)
+		}
 	}
 
 	// could be an invoice
