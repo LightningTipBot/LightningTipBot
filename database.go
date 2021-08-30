a5943f5ff681c8157b94a4f53f4bf79475cb7c45
LightningTipBot <hWOofXvq4m@mail.com>
2021-08-30 15:37:42 +0200

    save lowercase user

--- database.go
+++ database.go
@@ -4,7 +4,6 @@ import (
 	"fmt"
 	"reflect"
 	"strconv"
-	"strings"
 
 	log "github.com/sirupsen/logrus"
 
@@ -50,11 +49,10 @@ func GetUser(u *tb.User, bot TipBot) (*lnbits.User, error) {
 	}()
 	var err error
 	go func() {
-		u_cp := *u
-		u_cp.Username = strings.ToLower(u.Username)
+		u_cp := bot.copyLowercaseUser(u)
 		if !reflect.DeepEqual(u_cp, user.Telegram) {
 			// update possibly changed user details in database
-			user.Telegram = &u_cp
+			user.Telegram = u_cp
 			err = UpdateUserRecord(user, bot)
 			if err != nil {
 				log.Warnln(fmt.Sprintf("[UpdateUserRecord] %s", err.Error()))
@@ -65,6 +63,7 @@ func GetUser(u *tb.User, bot TipBot) (*lnbits.User, error) {
 }
 
 func UpdateUserRecord(user *lnbits.User, bot TipBot) error {
+	user.Telegram = bot.copyLowercaseUser(user.Telegram)
 	tx := bot.database.Save(user)
 	if tx.Error != nil {
 		errmsg := fmt.Sprintf("[UpdateUserRecord] Error: Couldn't update %s's info in database.", GetUserStr(user.Telegram))
