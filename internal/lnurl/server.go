aa6090d34aff257f2d56025ccfda035ceb6c06fa
gohumble <deniz.hornig@internetx.com>
2021-09-03 21:14:11 +0200

    restructure lnurl server

--- internal/lnurl/server.go
+++ internal/lnurl/server.go
@@ -30,6 +30,9 @@ type Server struct {
 }
 
 const (
+	statusError   = "ERROR"
+	statusOk      = "OK"
+	payRequestTag = "payRequest"
 	lnurlEndpoint = "/.well-known/lnurlp"
 	minSendable   = 1000 // mSat
 	MaxSendable   = 1000000000
@@ -65,143 +68,160 @@ func (w *Server) newRouter() *mux.Router {
 	router := mux.NewRouter()
 	router.HandleFunc(lnurlEndpoint+"/{username}", w.handleLnUrl).Methods(http.MethodGet)
 	router.HandleFunc("/@{username}", w.handleLnUrl).Methods(http.MethodGet)
-	//router.HandleFunc("/.well-know/lnurlp/{username}", w.handleLnUrl).Methods(http.MethodGet)
 	return router
 }
 
 func (w Server) handleLnUrl(writer http.ResponseWriter, request *http.Request) {
+	var err error
+	var response interface{}
+	username := mux.Vars(request)["username"]
 	if request.URL.RawQuery == "" {
-		var err error
-		err = w.serveLNURLpFirst(writer, request)
-		if err != nil {
-			log.Errorln(err)
+		response, err = w.serveLNURLpFirst(username)
+	} else {
+		stringAmount := request.FormValue("amount")
+		if stringAmount == "" {
+			NotFoundHandler(writer, fmt.Errorf("[serveLNURLpSecond] Form value 'amount' is not set"))
 			return
 		}
-	} else {
-		w.serveLNURLpSecond(writer, request)
+		amount, err := strconv.Atoi(stringAmount)
+		if err != nil {
+			NotFoundHandler(writer, fmt.Errorf("[serveLNURLpSecond] Couldn't cast amount to int %v", err))
+			return
+		}
+		response, err = w.serveLNURLpSecond(username, int64(amount))
 	}
+	if err != nil {
+		log.Errorln(err)
+		if response != nil {
+			err = writeResponse(writer, response)
+			if err != nil {
+				NotFoundHandler(writer, err)
+			}
+		}
+	}
+	err = writeResponse(writer, response)
+	if err != nil {
+		NotFoundHandler(writer, err)
+
+	}
+}
+func NotFoundHandler(writer http.ResponseWriter, err error) {
+	log.Errorln(err)
+	// return 404 on any error
+	http.Error(writer, "404 page not found", http.StatusNotFound)
 }
 
 // descriptionHash is the SHA256 hash of the metadata
-func (w Server) descriptionHash(metadata lnurl.Metadata, writer http.ResponseWriter) string {
+func (w Server) descriptionHash(metadata lnurl.Metadata) (string, error) {
 	jsonMeta, err := json.Marshal(metadata)
 	if err != nil {
-		writer.WriteHeader(400)
-		return ""
+		return "", err
 	}
 	hash := sha256.Sum256([]byte(string(jsonMeta)))
-	hash_string := hex.EncodeToString(hash[:])
-	return hash_string
+	hashString := hex.EncodeToString(hash[:])
+	return hashString, nil
 }
 
 // metaData returns the metadata that is sent in the first response
 // and is used again in the second response to verify the description hash
-func (w Server) metaData(request *http.Request) lnurl.Metadata {
-	vars := mux.Vars(request)
-	return lnurl.Metadata{{"text/identifier", fmt.Sprintf("%s@ln.tips", vars["username"])}, {"text/plain", fmt.Sprintf("Pay to %s@%s", vars["username"], w.callbackUrl)}}
+func (w Server) metaData(username string) lnurl.Metadata {
+
+	return lnurl.Metadata{
+		{"text/identifier", fmt.Sprintf("%s@ln.tips", username)},
+		{"text/plain", fmt.Sprintf("Pay to %s@%s", username, w.callbackUrl)}}
 }
 
 // serveLNURLpFirst serves the first part of the LNURLp protocol with the endpoint
 // to call and the metadata that matches the description hash of the second response
-func (w Server) serveLNURLpFirst(writer http.ResponseWriter, request *http.Request) error {
-	vars := mux.Vars(request)
-	callback := fmt.Sprintf("%s%s/%s", w.callbackUrl, lnurlEndpoint, vars["username"])
-	callbackURL, err := url.Parse(callback)
+func (w Server) serveLNURLpFirst(username string) (*lnurl.LNURLPayResponse1, error) {
+
+	log.Infof("[LNURL] Serving endpoint for user %s", username)
+	callbackURL, err := url.Parse(fmt.Sprintf("https://%s%s/%s", w.callbackUrl, lnurlEndpoint, username))
 	if err != nil {
-		log.Errorln("callback is not a valid URL")
-		writer.WriteHeader(400)
-		return err
+		return nil, err
 	}
-	metadata := w.metaData(request)
+	metadata := w.metaData(username)
 	jsonMeta, err := json.Marshal(metadata)
 	if err != nil {
-		writer.WriteHeader(400)
-		return err
+		return nil, err
 	}
 
-	resp := lnurl.LNURLPayResponse1{
-		LNURLResponse:   lnurl.LNURLResponse{Status: "OK"},
-		Tag:             "payRequest",
-		Callback:        fmt.Sprintf("https://%s", callback),
-		CallbackURL:     callbackURL,
+	return &lnurl.LNURLPayResponse1{
+		LNURLResponse:   lnurl.LNURLResponse{Status: statusOk},
+		Tag:             payRequestTag,
+		Callback:        callbackURL.String(),
+		CallbackURL:     callbackURL, // probably no need to set this here
 		MinSendable:     minSendable,
 		MaxSendable:     MaxSendable,
 		EncodedMetadata: string(jsonMeta),
-	}
-	jsonResponse, err := json.Marshal(resp)
-	if err != nil {
-		writer.WriteHeader(400)
-		return err
-	}
-	writer.WriteHeader(200)
-	writer.Write(jsonResponse)
-	log.Infof("[LNURL] Serving endpoint for user %s", vars["username"])
-	return nil
+	}, nil
+
 }
 
 // serveLNURLpSecond serves the second LNURL response with the payment request with the correct description hash
-func (w Server) serveLNURLpSecond(writer http.ResponseWriter, request *http.Request) {
-	vars := mux.Vars(request)
+func (w Server) serveLNURLpSecond(username string, amount int64) (*lnurl.LNURLPayResponse2, error) {
+
+	log.Infof("[LNURL] Serving invoice for user %s", username)
+
 	user := &lnbits.User{}
-	tx := w.database.Where("telegram_username = ?", strings.ToLower(vars["username"])).First(user)
-	if tx.Error != nil || user.Wallet == nil || user.Initialized == false {
-		errmsg := fmt.Sprintf("[GetUser] Couldn't fetch user info from database.")
-		log.Warnln(errmsg)
-		return
+	tx := w.database.Where("telegram_username = ?", strings.ToLower(username)).First(user)
+	if tx.Error != nil {
+		return nil, fmt.Errorf("[GetUser] Couldn't fetch user info from database: %v", tx.Error)
+	}
+	if user.Wallet == nil || user.Initialized == false {
+		return nil, fmt.Errorf("[serveLNURLpSecond] invalid user data")
 	}
 
 	// set wallet lnbits client
 	user.Wallet.Client = w.c
-	stringAmount := request.FormValue("amount")
-	amount, err := strconv.Atoi(stringAmount)
-	if err != nil {
-		errmsg := fmt.Sprintf("[serveLNURLpSecond] Couldn't cast amount to int")
-		log.Warnln(errmsg)
-		return
-	}
 
-	var resp lnurl.LNURLPayResponse2
+	var resp *lnurl.LNURLPayResponse2
 
 	if amount < minSendable || amount > MaxSendable {
 		// amount is not ok
-		errmsg := fmt.Sprintf("[serveLNURLpSecond] Amount out of bounds")
-		log.Warnln(errmsg)
-		resp = lnurl.LNURLPayResponse2{
-			LNURLResponse: lnurl.LNURLResponse{Status: "ERROR", Reason: fmt.Sprintf("Amount out of bounds (min: %d mSat, max: %d mSat).", minSendable, MaxSendable)},
-		}
-	} else {
-		// amount is ok
-
-		// the same description_hash needs to be built in the second request
-		metadata := w.metaData(request)
-		hash_string := w.descriptionHash(metadata, writer)
-		invoice, err := user.Wallet.Invoice(
-			lnbits.InvoiceParams{
-				Amount:          int64(amount / 1000),
-				Out:             false,
-				DescriptionHash: hash_string,
-				Webhook:         w.WebhookServer},
-			*user.Wallet)
-		if err != nil {
-			errmsg := fmt.Sprintf("[serveLNURLpSecond] Couldn't create invoice: %s", err.Error())
-			log.Warnln(errmsg)
-			resp = lnurl.LNURLPayResponse2{
-				LNURLResponse: lnurl.LNURLResponse{Status: "ERROR", Reason: "Couldn't create invoice."},
-			}
-		} else {
-			resp = lnurl.LNURLPayResponse2{
-				LNURLResponse: lnurl.LNURLResponse{Status: "OK"},
-				PR:            invoice.PaymentRequest,
-				Routes:        make([][]lnurl.RouteInfo, 0),
-				SuccessAction: &lnurl.SuccessAction{Message: "Payment received!", Tag: "message"},
-			}
-		}
+		return &lnurl.LNURLPayResponse2{
+			LNURLResponse: lnurl.LNURLResponse{
+				Status: statusError,
+				Reason: fmt.Sprintf("Amount out of bounds (min: %d mSat, max: %d mSat).", minSendable, MaxSendable)},
+		}, fmt.Errorf("amount out of bounds")
 	}
-	log.Infof("[LNURL] Serving invoice for user %s", vars["username"])
-	jsonResponse, err := json.Marshal(resp)
+	// amount is ok
+	// the same description_hash needs to be built in the second request
+	metadata := w.metaData(username)
+	descriptionHash, err := w.descriptionHash(metadata)
 	if err != nil {
-		writer.WriteHeader(400)
-		return
+		return nil, err
 	}
-	writer.Write(jsonResponse)
+	invoice, err := user.Wallet.Invoice(
+		lnbits.InvoiceParams{
+			Amount:          amount / 1000,
+			Out:             false,
+			DescriptionHash: descriptionHash,
+			Webhook:         w.WebhookServer},
+		*user.Wallet)
+	if err != nil {
+		err = fmt.Errorf("[serveLNURLpSecond] Couldn't create invoice: %v", err)
+		resp = &lnurl.LNURLPayResponse2{
+			LNURLResponse: lnurl.LNURLResponse{
+				Status: statusError,
+				Reason: "Couldn't create invoice."},
+		}
+		return resp, err
+	}
+	return &lnurl.LNURLPayResponse2{
+		LNURLResponse: lnurl.LNURLResponse{Status: statusOk},
+		PR:            invoice.PaymentRequest,
+		Routes:        make([][]lnurl.RouteInfo, 0),
+		SuccessAction: &lnurl.SuccessAction{Message: "Payment received!", Tag: "message"},
+	}, nil
+
+}
+
+func writeResponse(writer http.ResponseWriter, response interface{}) error {
+	jsonResponse, err := json.Marshal(response)
+	if err != nil {
+		return err
+	}
+	_, err = writer.Write(jsonResponse)
+	return err
 }
