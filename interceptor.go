4f228321a3a11874835a38bc00551623d9de6f5b
lngohumble <lngohumble@gmail.com>
2021-09-19 22:32:22 +0200

    check type in interceptor

--- interceptor.go
+++ interceptor.go
@@ -16,6 +16,8 @@ const (
 	QueryInterceptor
 )
 
+var invalidTypeError = fmt.Errorf("invalid type")
+
 type Interceptor struct {
 	Type           InterceptorType
 	BeforeMessage  []intercept.Func
@@ -41,7 +43,7 @@ func (bot TipBot) requireUserInterceptor(ctx context.Context, i interface{}) (co
 		user, err := GetUser(i.(*tb.Message).Sender, bot)
 		return context.WithValue(ctx, "user", user), err
 	}
-	return nil, fmt.Errorf("invalid type")
+	return nil, invalidTypeError
 }
 func (bot TipBot) loadUserInterceptor(ctx context.Context, i interface{}) (context.Context, error) {
 	ctx, _ = bot.requireUserInterceptor(ctx, i)
@@ -50,23 +52,31 @@ func (bot TipBot) loadUserInterceptor(ctx context.Context, i interface{}) (conte
 
 // loadReplyToInterceptor Loading the telegram user with message intercept
 func (bot TipBot) loadReplyToInterceptor(ctx context.Context, i interface{}) (context.Context, error) {
-	m := i.(*tb.Message)
-	if m.ReplyTo != nil {
-		if m.ReplyTo.Sender != nil {
-			user, _ := GetUser(m.ReplyTo.Sender, bot)
-			user.Telegram = m.ReplyTo.Sender
-			return context.WithValue(ctx, "reply_to_user", user), nil
+	switch i.(type) {
+	case *tb.Message:
+		m := i.(*tb.Message)
+		if m.ReplyTo != nil {
+			if m.ReplyTo.Sender != nil {
+				user, _ := GetUser(m.ReplyTo.Sender, bot)
+				user.Telegram = m.ReplyTo.Sender
+				return context.WithValue(ctx, "reply_to_user", user), nil
+			}
 		}
+		return context.Background(), fmt.Errorf("reply to user not found")
 	}
-	return context.Background(), fmt.Errorf("reply to user not found")
+	return nil, invalidTypeError
 }
 
 func (bot TipBot) requirePrivateChatInterceptor(ctx context.Context, i interface{}) (context.Context, error) {
-	m := i.(*tb.Message)
-	if m.Chat.Type != tb.ChatPrivate {
-		return nil, fmt.Errorf("no private chat")
+	switch i.(type) {
+	case *tb.Message:
+		m := i.(*tb.Message)
+		if m.Chat.Type != tb.ChatPrivate {
+			return nil, fmt.Errorf("no private chat")
+		}
+		return ctx, nil
 	}
-	return ctx, nil
+	return nil, invalidTypeError
 }
 
 // LoadUser from context
