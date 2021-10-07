286f2705c9e5a9a317ea683fc3255220d00b7672
LightningTipBot <hWOofXvq4m@mail.com>
2021-10-08 02:22:53 +0300

    more currencies

--- internal/price/price.go
+++ internal/price/price.go
@@ -17,24 +17,29 @@ import (
 type PriceWatcher struct {
 	client         *http.Client
 	UpdateInterval time.Duration
-	Currencies     []string
+	Currencies     map[string]string
 	Exchanges      map[string]func(string) (float64, error)
-	Price          map[string]float64
 }
+
+var (
+	Price map[string]float64
+	P     *PriceWatcher
+)
 
 func NewPriceWatcher() *PriceWatcher {
 	pricewatcher := &PriceWatcher{
 		client: &http.Client{
 			Timeout: time.Second * time.Duration(5),
 		},
-		Currencies:     []string{"USD", "EUR"},
-		Price:          make(map[string]float64, 0),
+		Currencies:     map[string]string{"EUR": "€", "GBP": "£", "JPY": "¥", "BRL": "R$", "USD": "$", "RUB": "₽", "TRY": "₺"},
 		Exchanges:      make(map[string]func(string) (float64, error), 0),
 		UpdateInterval: time.Second * time.Duration(30),
 	}
 	pricewatcher.Exchanges["coinbase"] = pricewatcher.GetCoinbasePrice
 	pricewatcher.Exchanges["bitfinex"] = pricewatcher.GetBitfinexPrice
+	Price = make(map[string]float64, 0)
 	log.Infof("[PriceWatcher] Watcher started")
+	P = pricewatcher
 	return pricewatcher
 }
 
@@ -44,24 +49,25 @@ func (p *PriceWatcher) Start() {
 
 func (p *PriceWatcher) Watch() error {
 	for {
-		time.Sleep(p.UpdateInterval)
-		for _, currency := range p.Currencies {
+		for currency, _ := range p.Currencies {
 			avg_price := 0.0
 			n_responses := 0
 			for exchange, getPrice := range p.Exchanges {
 				fprice, err := getPrice(currency)
 				if err != nil {
-					log.Error(err)
+					// log.Debug(err)
 					// if one exchanges is down, use the next
 					continue
 				}
 				n_responses++
 				avg_price += fprice
-				log.Debugf("[PriceWatcher] %s %s price: %f", exchange, currency, fprice)
+				log.Infof("[PriceWatcher] %s %s price: %f", exchange, currency, fprice)
+				time.Sleep(time.Second * time.Duration(2))
 			}
-			p.Price[currency] = avg_price / float64(n_responses)
-			log.Debugf("[PriceWatcher] Average %s price: %f", currency, p.Price[currency])
+			Price[currency] = avg_price / float64(n_responses)
+			log.Debug("[PriceWatcher] Average %s price: %f", currency, Price[currency])
 		}
+		time.Sleep(p.UpdateInterval)
 	}
 }
 
@@ -73,18 +79,20 @@ func (p *PriceWatcher) GetCoinbasePrice(currency string) (float64, error) {
 	}
 	bodyBytes, err := ioutil.ReadAll(response.Body)
 	if err != nil {
-		log.Fatal(err)
+		log.Debug(err)
+		return 0, err
 	}
 	price := gjson.Get(string(bodyBytes), "data.amount")
 	fprice, err := strconv.ParseFloat(strings.TrimSpace(price.String()), 64)
 	if err != nil {
-		log.Fatal(err)
+		log.Debug(err)
+		return 0, err
 	}
 	return fprice, nil
 }
 
 func (p *PriceWatcher) GetBitfinexPrice(currency string) (float64, error) {
-	var bitfinexCurrencyToPair = map[string]string{"USD": "btcusd", "EUR": "btceur"}
+	var bitfinexCurrencyToPair = map[string]string{"USD": "btcusd", "EUR": "btceur", "GBP": "btcusd", "JPY": "btcjpy", "BRL": "btcbrl", "RUB": "btcrub", "TRY": "btctry"}
 	pair := bitfinexCurrencyToPair[currency]
 	bitfinexEndpoint, err := url.Parse(fmt.Sprintf("https://api.bitfinex.com/v1/pubticker/%s", pair))
 	response, err := p.client.Get(bitfinexEndpoint.String())
@@ -93,12 +101,14 @@ func (p *PriceWatcher) GetBitfinexPrice(currency string) (float64, error) {
 	}
 	bodyBytes, err := ioutil.ReadAll(response.Body)
 	if err != nil {
-		log.Fatal(err)
+		log.Debug(err)
+		return 0, err
 	}
 	price := gjson.Get(string(bodyBytes), "last_price")
 	fprice, err := strconv.ParseFloat(strings.TrimSpace(price.String()), 64)
 	if err != nil {
-		log.Fatal(err)
+		log.Debug(err)
+		return 0, err
 	}
 	return fprice, nil
 }
