e0708a14cedeb03867fa041dc84a26bf9da53c2b
callebtc <93376500+callebtc@users.noreply.github.com>
2021-11-15 16:37:04 +0100

    turn on ru.toml

--- internal/i18n/localize.go
+++ internal/i18n/localize.go
@@ -17,6 +17,7 @@ func RegisterLanguages() *i18n.Bundle {
 	bundle := i18n.NewBundle(language.English)
 	bundle.RegisterUnmarshalFunc("toml", toml.Unmarshal)
 	bundle.MustLoadMessageFile("translations/en.toml")
+	bundle.MustLoadMessageFile("translations/ru.toml")
 	bundle.LoadMessageFile("translations/de.toml")
 	bundle.LoadMessageFile("translations/it.toml")
 	bundle.LoadMessageFile("translations/es.toml")
