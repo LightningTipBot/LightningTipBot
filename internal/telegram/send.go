8b7166c8c8682a65dbcb7eabeb0502f54ebc450e
lngohumble <lngohumble@gmail.com>
2021-12-23 15:35:26 +0100

    add mutex package

--- internal/telegram/send.go
+++ internal/telegram/send.go
@@ -4,6 +4,7 @@ import (
 	"context"
 	"encoding/json"
 	"fmt"
+	"github.com/LightningTipBot/LightningTipBot/internal/runtime/mutex"
 	"strings"
 
 	"github.com/LightningTipBot/LightningTipBot/internal/i18n"
@@ -214,6 +215,8 @@ func (bot *TipBot) sendHandler(ctx context.Context, m *tb.Message) {
 // sendHandler invoked when user clicked send on payment confirmation
 func (bot *TipBot) confirmSendHandler(ctx context.Context, c *tb.Callback) {
 	tx := &SendData{Base: transaction.New(transaction.ID(c.Data))}
+	mutex.Lock(tx.ID)
+	defer mutex.Unlock(tx.ID)
 	sn, err := tx.Get(tx, bot.Bunt)
 	if err != nil {
 		log.Errorf("[acceptSendHandler] %s", err)
@@ -310,7 +313,7 @@ func (bot *TipBot) cancelSendHandler(ctx context.Context, c *tb.Callback) {
 		log.Errorf("[acceptSendHandler] %s", err)
 		return
 	}
-	defer transaction.Unlock(tx.ID)
+	defer mutex.Unlock(tx.ID)
 
 	sendData := sn.(*SendData)
 	// onnly the correct user can press
