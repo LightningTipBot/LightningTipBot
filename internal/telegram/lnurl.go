e896c489da5d1d0244db9363dde0b01a776b6a21
callebtc <93376500+callebtc@users.noreply.github.com>
2022-05-02 23:24:50 +0200

    remove comments

--- internal/telegram/lnurl.go
+++ internal/telegram/lnurl.go
@@ -22,45 +22,6 @@ import (
 	tb "gopkg.in/lightningtipbot/telebot.v3"
 )
 
-// func (bot *TipBot) GetHttpClient() (*http.Client, error) {
-// 	client := http.Client{
-// 		Timeout: 10 * time.Second,
-// 	}
-// 	if internal.Configuration.Bot.HttpProxy != "" {
-// 		proxyUrl, err := url.Parse(internal.Configuration.Bot.HttpProxy)
-// 		if err != nil {
-// 			log.Errorln(err)
-// 			return nil, err
-// 		}
-// 		client.Transport = &http.Transport{Proxy: http.ProxyURL(proxyUrl)}
-// 	}
-// 	return &client, nil
-// }
-// func (bot *TipBot) GetSocksClient() (*http.Client, error) {
-// 	client := http.Client{
-// 		Timeout: 10 * time.Second,
-// 	}
-// 	if internal.Configuration.Bot.SocksProxy != "" {
-// 		proxyURL, _ := url.Parse(internal.Configuration.Bot.SocksProxy)
-// 		specialTransport := &http.Transport{}
-// 		specialTransport.Proxy = http.ProxyURL(proxyURL)
-// 		d, err := proxy.SOCKS5("tcp", internal.Configuration.Bot.SocksProxy, nil, &net.Dialer{
-// 			Timeout:   20 * time.Second,
-// 			Deadline:  time.Now().Add(time.Second * 10),
-// 			KeepAlive: -1,
-// 		})
-// 		if err != nil {
-// 			log.Errorln(err)
-// 			return &client, nil
-// 		}
-// 		specialTransport.DialContext = func(ctx context.Context, network, addr string) (net.Conn, error) {
-// 			return d.Dial(network, addr)
-// 		}
-// 		client.Transport = specialTransport
-// 	}
-// 	return &client, nil
-// }
-
 func (bot TipBot) cancelLnUrlHandler(c *tb.Callback) {
 }
 
